2dd8bb77a1f2b4ffa3082143de9be39355f548bd
k896951 <git@hgotoh.jp>
2021-04-03 18:00:43 +0900

    DataContractJsonSerializer()がnullを返す事への対処。説明されて納得した。

--- FakeChan/Properties/Settings.Designer.cs
+++ FakeChan/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace FakeChan.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("{}")]
         public string UserSettings {
             get {
                 return ((string)(this["UserSettings"]));
