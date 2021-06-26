3f2ecc8d4b2497df5e99f12ff2471daac4555bb1
k896951 <git@hgotoh.jp>
2021-06-27 04:11:46 +0900

    マクロサポートやり直し

--- FakeChan/MainWindow.xaml.cs
+++ FakeChan/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace FakeChan
     public partial class MainWindow : Window
     {
         string titleStr = "偽装ちゃん";
-        string versionStr = "Ver 2.0.8";
+        string versionStr = "Ver 2.0.8.2";
         MessQueueWrapper MessQueWrapper = new MessQueueWrapper();
         Configs Config;
         IpcTasks IpcTask = null;
